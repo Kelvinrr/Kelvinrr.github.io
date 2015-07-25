868a3048ca112d54c99c3d41fb7be638a9e39094
Kelvin Rodriguez <kelvinrr@icloud.com>
2015-07-24 17:19:03 -0700

    Added first stylesheet

---
+++ Assets/SytleSheets/in-contruction.js
@@ -0,0 +1,11 @@
+.jumbotron {
+  background-color: #26CBE0;
+  height: 500px;
+}
+
+.jumbotron h1 {
+  color: #E6CD69;
+  font-size: 48px;
+  font-family: 'Shift', sans-serif;
+  font-weight: bold;
+}
