273abcb6c0bf4582ac8b3e455e7b31ae045c61bf
Kelvin Rodriguez <kelvinrr@icloud.com>
2015-07-24 18:00:17 -0700

    renamed directories

---
+++ Assets/SytleSheets/in-contruction.js
@@ -0,0 +1,11 @@
+.jumbotron {
+  background-color: #26CBE0;
+  height: 500px;
+}
+
+.jumbotron h1 {
+  color: #E6CD69;
+  font-size: 80px;
+  font-family: 'Shift', sans-serif;
+  font-weight: bold;
+}
